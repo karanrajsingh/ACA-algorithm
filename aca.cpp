4e28d1c80f54abfd3ebd5584f67bd1d353a6f97b
karanrajsingh <singhkaranraj007@gmail.com>
2016-06-15 11:20:44 +0530

    Update aca.cpp

--- aca.cpp
+++ aca.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 using namespace Eigen;
 
-#define max_row 3
-#define max_col 3
+#define max_row 100
+#define max_col 10
 
 template<typename Derived, typename OtherDerived>
 double abs_scalar(MatrixBase<Derived>& U, MatrixBase<OtherDerived>& V, long int rank){
@@ -15,17 +15,15 @@ double abs_scalar(MatrixBase<Derived>& U, MatrixBase<OtherDerived>& V, long int 
 	} else{
 		double sum = 0;
 		for (long int i=0; i < (rank-1);  i++ ){
-
 			sum = sum + abs((U.col(i).transpose()*U.col(rank-1))(0,0)*(V.row(rank-1)*V.row(i).transpose())(0,0));
-
 		}
 		return(2*sum);
 	}
 }
 
-template <typename Derived, typename OtherDerived>
-long int max_index(const MatrixBase<Derived>& M, const MatrixBase<OtherDerived>& N, long int vector_max_size){
-	long int i,j,k;
+template <typename Derived, typename OtherDerived, typename Dummy>
+long int max_index(const MatrixBase<Derived>& M, const MatrixBase<OtherDerived>& N, const MatrixBase<Dummy>& D, long int vector_max_size){
+	long int i,j,k,l;
 	j=-1;
 	if(N.size()==1){
 		double max = abs(M(0));
@@ -46,8 +44,13 @@ long int max_index(const MatrixBase<Derived>& M, const MatrixBase<OtherDerived>&
 				if((N(k)==i)){
 					flag = false;
 				}
-
 			}
+			for(l=0;l<D.size();l++){
+				if(D(l)==i){
+					flag = false;
+				}
+			}
+
 			if(flag && (abs(M(i))>max || abs(M(i))==max)){
 				max = abs(M(i));
 				j=i;
@@ -59,9 +62,12 @@ long int max_index(const MatrixBase<Derived>& M, const MatrixBase<OtherDerived>&
 
 int main(){
 
-	MatrixXd Z(max_row, max_col);
-	//Initialize Z
+	float tol;
+	cout<<"Enter tolerance: "<<endl;
+	cin>>tol;
 
+	MatrixXd Z(max_row, max_col);
+	//Intializing Z
 	cout<<"Enter the matrix entries:"<<endl;
 	for(int x=0; x<max_row; x++){
 		for(int y=0; y<max_col; y++){
@@ -73,33 +79,35 @@ int main(){
 
 	MatrixXd Z_approx(max_row, max_col);
 	MatrixXd R(max_row, max_col);
-
+	
 	long int k = 0;
 
 	RowVectorXi I(k);
 	VectorXi J(k);
+	VectorXi D(k);
+	VectorXi A(k);
 
 	MatrixXd U(max_row, k);
 	MatrixXd V(k, max_col);
 
 	Z_approx = MatrixXd::Zero(max_row, max_col);
 	double z = Z_approx.squaredNorm();
-	
+		
 	//Randomly initialization of epsilon's value
 	float epsilon = -10;
 
-	while(abs(epsilon) > 0.001 && k<max_row && k<max_col){
+	while(abs(epsilon) >tol && k<max_row && k<max_col){
 	
 		cout<<endl<<"Beginning iteration: "<<(k+1)<<endl;
 
 		I.conservativeResize(k+1);
-		//Setting index as -1 to avoid random assignments due to resize
-		I(k) = -1;
+		//Setting index as -2 to avoid random assignments due to resize
+		I(k) = -2;
 	
 		if(J.size() == 0){
 			I(k) = k;
 		}else{
-			I(k) = max_index(R.col(J(k-1)), I, max_row);
+			I(k) = max_index(R.col(J(k-1)), I, A, max_row);
 		}
 		if(I(k) == -1){ 
 			break;	
@@ -107,49 +115,63 @@ int main(){
 			R.row(I(k)) = Z.row(I(k));
 			for(int d=0;d<k;d++){ 
 				R.row(I(k)) = R.row(I(k)) - U(I(k),d)*V.row(d);
-
 			}
 		}
-	
+		
 		J.conservativeResize(k+1);
-		J(k) = -1;
-		J(k) = max_index(R.row(I(k)), J, max_col);
-		cout<<endl<<"I:"<<endl<<I<<endl<<endl<<"J:"<<endl<<J<<endl;
-	
-		V.conservativeResize(k+1,max_col);
-		U.conservativeResize(max_row,k+1);
-	
-		//Setting index as -1 to avoid random assignments due to resize
+		//Setting index as -2 to avoid random assignments due to resize
+		J(k) = -2;
+		J(k) = max_index(R.row(I(k)), J, A, max_col);
+		bool flag = false;
 		if(J(k) == -1){
 			break;
 		}else{
-			V.row(k)= R.row(I(k))/R(I(k),J(k));
-			R.col(J(k)) = Z.col(J(k));
-			for(int d=0;d<k;d++){
-				R.col(J(k)) = R.col(J(k)) - V(d,J(k))*U.col(d);
+			int j;
+			if(abs(R(I(k),J(k)))<tol){
+				while(abs(R(I(k),J(k)))<tol){
+					j = D.size();
+					D.conservativeResize(j+1);
+					D(j)=I(k);
+					I(k) = max_index(R.col(J(k-1)), I, D, max_row); 
+					for(int d=0;d<k;d++){
+						R.row(I(k)) = R.row(I(k)) - U(I(k),d)*V.row(d);
+					}
+					J(k) = max_index(R.row(I(k)), J, A, max_col);
+					if(J(k) == -1 || I(k) == -1 || !(j<(max_row-k-1) && j<(max_col-k-1))){
+						flag = true;
+						break;
+					}
+				}
 			}
-		}
-	
-		U.col(k) = R.col(J(k));
-		cout<<endl<<"V:"<<endl<<V<<endl<<endl<<"U:"<<endl<<U<<endl;
+			if(flag || J(k) == -1 || I(k) == -1){
+				break;
+			}
+
+			V.conservativeResize(k+1,max_col);	
+				V.row(k)= R.row(I(k))/R(I(k),J(k));
+					R.col(J(k)) = Z.col(J(k));
+					for(int d=0;d<k;d++){
+						R.col(J(k)) = R.col(J(k)) - V(d,J(k))*U.col(d);
+					}
+			}
 		
-		cout<<endl<<"V.row("<<k<<")"<<V.row(k)<<endl<<"U.col("<<k<<")"<<U.col(k)<<endl;
-
+		U.conservativeResize(max_row,k+1);
+		U.col(k) = R.col(J(k));
+	
 		z = z + V.row(k).squaredNorm()*U.col(k).squaredNorm() +abs_scalar(U,V,k+1);
-
-		cout<<endl<<"Squared Norm: "<<z<<endl;
 	
 		epsilon = (U.col(k).norm()*V.row(k).norm())/sqrt(z);
 		cout<<endl<<"epsilon= "<<epsilon<<endl;
-	
-		cout<<endl<<"Approximate Error Matrix: "<<endl<<R<<endl;
-		cout<<endl<<"V.rows: "<<V.rows()<<endl<<"U.col: "<<U.cols()<<endl<<"rank: "<<k+1<<endl;
 		cout<<endl<<"Ending iteration: "<<(k+1)<<endl;
 	
 		k++;
 	}
-	
+
+	cout<<endl<<"U:"<<endl<<U<<endl<<"V:"<<endl<<V<<endl;
 	cout<<endl<<"U*V"<<endl<<U*V<<endl;
-	
+	cout<<endl<<endl<<"rank of the matrix:"<<endl<<k<<endl;	
+	cout<<endl<<"I:"<<endl<<I<<endl<<endl<<"J:"<<endl<<J<<endl;
+	cout<<endl<<"Norm(Z-U*V)/Norm(Z)"<<endl<<(Z-U*V).norm()/Z.norm()<<endl;	
+	cout<<endl<<"Norm(R)/Norm(Z)"<<endl<<R.norm()/Z.norm()<<endl;
 	return 0;
 }
